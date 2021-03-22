73043abf4ac2b379f86e2d022a151350dd8d44fd
Talon Walz <78514227+talonwalz@users.noreply.github.com>
2021-03-22 14:04:19 -0600

    finished assignment + black diamond

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 export default class Student extends Component {
   constructor() {
     super()
-
+    this.state = {
+      studentInfo: {}
+    }
   }
-
+  componentDidMount() {
+    axios.get(`http://localhost:3005/students/${this.props.match.params.id}`)
+    .then(res => this.setState({studentInfo: res.data}))
+    .catch(err => console.log(err))
+  }
   render() {
+
     return (
       <div className="box">
-        <h1>Student</h1>
+        <Link to={`/classlist/${this.state.studentInfo.class}`}><button>Class List</button></Link>
+        <h1>{this.state.studentInfo.first_name} {this.state.studentInfo.last_name}</h1>
+        <h3>Grade: {this.state.studentInfo.grade}</h3>
+        <h3>Email: {this.state.studentInfo.email}</h3>
+        
       </div>
     )
   }
